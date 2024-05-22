5314a5052ed0560e6c7a980cdd0da7a1207cec91
jukki <jukki.snow@gmail.com>
2024-05-22 07:13:09 +0700

    save 22.05.2024

--- my_site.py
+++ my_site.py
@@ -1,38 +1,43 @@
-# -*- coding: 1251 -*-
-# from getpass import getpass
-from mysql.connector import connect, Error
-# import mysql
-import mysql.connector
+from psycopg2 import connect, Error
+import psycopg2
 import html
-from flask import Flask, render_template, request
-from DB_try import UseDatabase, ConnectionError, CredentialsError, SQLError
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-app.config['db_config'] = {'host': '127.0.0.1',
-                           'port': '3306',
-                           'user': 'Jukki',
-                           'password': 'carib637',
-                           'database': 'forever_glasses'}
+
+@app.route('/viewlog', methods=['GET', 'POST'])
+def view_log() -> 'html':
+    """Display the contents of the log file as a HTML table."""
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
-# def create_connection(user_host, user_port, user_name, passwd, db_name):
-#     cursor = connection.cursor()
-#     connection = create_connection(
-#         "127.0.0.1", "3306", "Jukki", "carib637", "forever_glasses")
-#     try:
-#         cursor.execute()
-#         connection = mysql.connector.connect(host=user_host,
-#                                              port=user_port,
-#                                              user=user_name,
-#                                              password=passwd,
-#                                              database=db_name,
-#                                              )
-#         print("Сongratulations_connection_'forever_glasses'")
+try:
+    connection = psycopg2.connect(host='127.0.0.1',
+                                  port='5432',
+                                  user='Jukki',
+                                  password='carib637',
+                                  database='forever_glasses',
+                                  )
 
-#     except Error as e:
-#         print(f"The error '{e}' occurred")
+    cursor = connection.cursor()
+    print("Info PostgreSQL")
+    print(connection.get_dsn_parameters(), "\n")
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You connection - ", record, "\n")
+
+except (Exception, Error) as error:
+    print("Error server PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("Connection PostgreSQL closed.")
 
 
 @app.route('/')
@@ -64,32 +69,6 @@ def work_gallery_4() -> 'html':
 @app.route('/gallery#5', methods=['GET', 'POST'])
 def work_gallery_5() -> 'html':
     return render_template('gallery_5.html')
-
-
-@app.route('/viewlog', methods=['GET', 'POST'])
-def view_log() -> 'html':
-    """Display the contents of the log file as a HTML table."""
-    try:
-        with UseDatabase(app.config['dbconfig']) as cursor:
-            _SQL = """select id, ip, datetime, browser_string from log"""
-            cursor.execute(_SQL)
-            contents = cursor.fetchall()
-        titles = ('id', 'ip', 'datetime', 'browser_string')
-        return render_template('viewlog.html',
-                               the_title='View Log',
-                               the_row_titles=titles,
-                               the_data=contents,
-                               )
-
-    except ConnectionError as err:
-        print('Is your database switched on? Error:', str(err))
-    except CredentialsError as err:
-        print('User-id/Password issues. Error:', str(err))
-    except SQLError as err:
-        print('Is your query correct? Error:', str(err))
-    except Exception as err:
-        print('Something went wrong:', str(err))
-    return 'Error'
 
 
 app.secret_key = "&jRdu%/dPPP#cLA#**bW%tt"
